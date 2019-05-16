92a43c39a73630cd8f2370d8785e5ba3672fce6a
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-16 18:43:31 +0200

    16.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,9 +1,6 @@
 from collections import Counter
-s = 'hahuythap'
-b = Counter(s)
-x = list(b.keys())
-y = list(b.values())
-print(x, y)
-
-new_list = [x for _,x in sorted(zip(y, x))]
-print(new_list)
+nums = [3,3,7,7,10,11,11]
+count = Counter(nums)
+print(count)
+res = list(count.keys())[list(count.values()).index(1)]
+print(res)
