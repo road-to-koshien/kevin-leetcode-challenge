77918860d432b3b0809668a727fcfe0ed88c1833
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-16 20:29:37 +0200

    16.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,6 +1,10 @@
-from collections import Counter
-nums = [3,3,7,7,10,11,11]
-count = Counter(nums)
-print(count)
-res = list(count.keys())[list(count.values()).index(1)]
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+res = list(car.items())
 print(res)
+
+x = [x for x,y  in res if y == "Ford"]
+print(x)
