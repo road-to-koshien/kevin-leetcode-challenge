b110d377fc9dc150b9c0ca3bbaef052060f50fff
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-16 12:34:08 +0200

    16.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,3 +1,11 @@
-s = ['h','k','a']
-s.pop(1)
-print(s.count('t'))
+import math
+points = [[3,3],[5,-1],[-2,4]]
+
+def get_distance(coord):
+    distance = math.sqrt(coord[0]**2 + coord[1]**2)
+    return distance
+length_list = []
+for each in points:
+    length_list.append(get_distance(each))
+points = [x for _, x in sorted(zip(length_list, points))]
+print(points)
