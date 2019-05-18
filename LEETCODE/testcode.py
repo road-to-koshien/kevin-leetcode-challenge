c57c9fcf7fc4c2ea56770d515f6a04a023fcf3d5
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-18 23:30:05 +0200

    18.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,24 +1,19 @@
+A = [2,1,2]
+def check_triangle(a,b,c):
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    else:
+        return False
+A.sort()
+A = A[::-1]
+print(A)
+i = 0
+while i+2 <= len(A) - 1:
+    if check_triangle(A[i], A[i+1], A[i+2]) == True:
+        print((A[i] + A[i+1] + A[i+2]))
+        break
+    else:
+        i +=1
+print(0)
 
-S = "a-bC-dEf-ghIj"
-S = list(S)
-i, j = 0, -1
-while i + abs(j) <= len(S)-1:
-    if S[i].isalpha() == True and S[j].isalpha() == True:
-        S[i], S[j] = S[j], S[i]
-        i += 1
-        j -= 1
-        continue
-    if S[i].isalpha() == False and S[j].isalpha() == False:
-        i += 1
-        j -= 1
-        continue
-    if S[i].isalpha() == True and S[j].isalpha() == False:
-        j -= 1
-        continue
-    if S[i].isalpha() == False and S[j].isalpha() == True:
-        i += 1
-        continue
-S = ''.join(S)
-print(S)
-
-# "j-Ih-gfE-dCba"
+# print(check_triangle(2,2,1))
