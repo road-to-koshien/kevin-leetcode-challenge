375fec8fe1f2f2b754313524cf61d1603f516737
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-20 06:56:42 +0200

    20.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,19 +1,14 @@
-A = [2,1,2]
-def check_triangle(a,b,c):
-    if a + b > c and b + c > a and a + c > b:
-        return True
-    else:
-        return False
-A.sort()
-A = A[::-1]
-print(A)
-i = 0
-while i+2 <= len(A) - 1:
-    if check_triangle(A[i], A[i+1], A[i+2]) == True:
-        print((A[i] + A[i+1] + A[i+2]))
-        break
-    else:
-        i +=1
-print(0)
+k = 5
+i = 3
+a = list(range(0, k+1))
 
-# print(check_triangle(2,2,1))
+
+res = [[]]
+final = []
+for each in a:
+    add = [i + [each] for i in res]
+    for item in add:
+        res.append(item)
+        if len(item) == i:
+            final.append(item)
+print(final)
