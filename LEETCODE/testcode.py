0f605494caa53fcf470c704dcc3fd86ac97dc4e3
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-28 15:17:32 +0200

    28.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,2 +1,14 @@
-a = [1,2,3,4]
-print(a.count(5))
+a = "AABAACAADAABAABA"
+b = 'AABA'
+res = []
+for i in range(len(a)):
+    t = 0
+    k = i
+    while a[k] == b[t]:
+        k += 1
+        if t == len(b) - 1:
+            res.append(i)
+            break
+        t += 1
+print(res)  
+
