896562ab09bcf1d7930723857a5f91bd51a21f04
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-29 15:58:04 +0200

    29.05.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,24 +1,16 @@
-customers = [1,0,1,2,1,1,7,5]
-grumpy =    [0,1,0,1,0,1,0,1]
-X = 3
-sum1 = 0
-sum2 = 0
-max = 0
-for i,each in enumerate(customers):
-    if grumpy[i] == 0:
-        sum1 += each
-        customers[i] = 0
-for i,each in enumerate(customers):
-    sum2 += each
-    if i >= X:
-        sum2 -= customers[i-X]
-    if sum2 > max:
-        max = sum2
-print(sum1 + max)
-
-
-
-
+A = [1,2]
+max1 = 0
+res = 0
+for i, each in enumerate(A):
+    res_cur = max1 + each - i
+    if res_cur > res:
+        res = res_cur
+    max_cur = A[i] + i
+    if max_cur > max1:
+        max1 = max_cur
+print(res)
+    
+     
 
 
 
