65be7b315fe5a5919656e3e6c6f87eeff349d736
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-30 08:07:49 +0200

    30.05.2019

---
+++ LEETCODE/728. Self Dividing Numbers.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/self-dividing-numbers/
+
+# A self-dividing number is a number that is divisible by every digit it contains.
+
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+# Also, a self-dividing number is not allowed to contain the digit zero.
+
+# Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+# Example 1:
+# Input: 
+# left = 1, right = 22
+# Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+# Note:
+
+# The boundaries of each input argument are 1 <= left <= right <= 10000.
+
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        def check(num):
+            a = [int(x) for x in list(str(num))]
+            for each in a:
+                if each == 0:
+                    return False
+                if num%each == 0:
+                    continue
+                else:
+                    return False
+            return True
+        res = []
+        for i in range(left, right+1):
+            if check(i):
+                res.append(i)
+        return res
+                
+        
--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,28 +1,3 @@
-A = [1,2]
-max1 = 0
-res = 0
-for i, each in enumerate(A):
-    res_cur = max1 + each - i
-    if res_cur > res:
-        res = res_cur
-    max_cur = A[i] + i
-    if max_cur > max1:
-        max1 = max_cur
-print(res)
-    
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a = '123'
+b = list(a)
+print(b)
