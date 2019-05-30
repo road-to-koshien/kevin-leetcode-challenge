cc8023702b6d711362d2328aeaa1306de0c7926c
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-05-30 22:17:29 +0200

    30.05.2019

---
+++ LEETCODE/349. Intersection of Two Arrays.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/
+
+# Given two arrays, write a function to compute their intersection.
+
+# Example 1:
+
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2]
+# Example 2:
+
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [9,4]
+# Note:
+
+# Each element in the result must be unique.
+# The result can be in any order.
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return set1.intersection(set2)
+        
