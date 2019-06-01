8e98151832945ebef6eafc4b2ad951e00c53d503
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-01 14:09:42 +0200

    01.06.2019

---
+++ LEETCODE/784. Letter Case Permutation.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/letter-case-permutation/
+
+# Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
+
+# Examples:
+# Input: S = "a1b2"
+# Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+
+# Input: S = "3z4"
+# Output: ["3z4", "3Z4"]
+
+# Input: S = "12345"
+# Output: ["12345"]
+# Note:
+
+# S will be a string with length between 1 and 12.
+# S will consist only of letters or digits.
+
+class Solution(object):
+    def letterCasePermutation(self, S):
+        res = ['']
+        for each in S:
+            if each.isalpha():
+                res = [i + j for i in res for j in [each.upper(), each.lower()]]
+            else:
+                res = [i + each for i in res]
+        return res
+                
+        
+        
--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,49 +1,10 @@
-# # Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices i 
-# # for which A[i] > B[i].
+# a = dict(one=1, two=2, three=3)
+# b = {'one': 1, 'two': 2, 'three': 3}
+# c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+# d = dict([('two', 2), ('one', 1), ('three', 3)])
+# e = dict({'three': 3, 'one': 1, 'two': 2})
+# print(a == b == c == d == e)
 
-# # Return any permutation of A that maximizes its advantage with respect to B.
-
-# # Example 1:
-
-# # Input: A = [2,7,11,15], B = [1,10,4,11]
-# # Output: [2,11,7,15]
-# # Example 2:
-
-# # Input: A = [12,24,8,32], B = [13,25,32,11]
-# # Output: [24,32,8,12]
-
-# A = [2,0,4,1,2]
-# B = [1,3,0,0,2]
-
-# # A = [12,24,8,32] 
-# # B = [13,25,32,11]
-# newdict = {}
-# res = []
-# x = sorted(A)
-# y = sorted(B)
-# minB = min(y)
-# for i,each in enumerate(x):
-#     if each > minB:
-#         k = i
-#         break
-# x1 = x[k:]
-# x2 = x[:k]
-# x_sorted = x1 + x2
-# for i,each in enumerate(x_sorted):
-#     if y[i] in newdict:
-#         newdict[y[i]].append(each)
-#     if y[i] not in newdict:
-#         newdict[y[i]] = [each]
-# for each in B:
-#     if len(newdict[each]) > 1:
-#         t = newdict[each]
-#         res.append(t.pop())
-#         newdict[each] = t
-#     else:
-#         res.append(newdict[each][0])
-# print(res)
-
-s = "loveleetcode"
-from collections import Counter
-x = list(Counter(s).keys()) 
-print(x)
+a = ['']
+b = ['a']
+print(a+b)
