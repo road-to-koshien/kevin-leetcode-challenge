b33e9c704320d16cb0b4d822fee506755d4a66ca
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-02 23:21:52 +0200

    02.06.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,22 +1,20 @@
-A = [54,64,85,85,35,42,16,59,39,70,31,37,66,16,24,56,67,13,42,46]
-B = [41,7,69,70,25,54,83,75,1,32,23,87,70,75,2,75,15,19,47,61]
-# sumA, sumB = 0, 0
-# newdict = {}
-# for each in A:
-#     sumA += each
-# for each in B:
-#     sumB += each
-# if sumA > sumB:
-#     A,B = B,A
-# equal = (sumA + sumB)/2
-# exchange = abs(sumA - equal)
-# for each in B:
-#     if each-exchange not in newdict:
-#         newdict[each-exchange] = each
-# for each in A:
-#     if each in newdict:
-#         print([each, newdict[each]])
-# print(newdict)
-# print(sumA, sumB, equal, exchange)
+A = [18,12,-18,18,-19,-1,10,10]
 
-print(sum(A))
+sums = sum(A)
+equal = sums / 3
+sum_temp = 0
+count = 0
+for each in A:
+    sum_temp = sum_temp + each
+    if sum_temp != equal:
+        continue
+    else:
+        count += 1
+        sum_temp = 0
+if count == 3:
+    print(True)
+else:
+    print(False)
+
+print(equal)
+print(count)
