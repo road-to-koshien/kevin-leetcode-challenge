c7e11f883943ab5352b5c4eb4c31cc15b4074c43
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-03 12:15:16 +0200

    03.06.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,20 +1,3 @@
-A = [18,12,-18,18,-19,-1,10,10]
-
-sums = sum(A)
-equal = sums / 3
-sum_temp = 0
-count = 0
-for each in A:
-    sum_temp = sum_temp + each
-    if sum_temp != equal:
-        continue
-    else:
-        count += 1
-        sum_temp = 0
-if count == 3:
-    print(True)
-else:
-    print(False)
-
-print(equal)
-print(count)
+nums = [1,2,3,4,5]
+median = sorted(nums)[len(nums) / 2]
+print(median)
