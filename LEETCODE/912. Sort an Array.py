9d4984aae8d19520a6a5d5110ca08e68f5f95328
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-04 23:26:11 +0200

    04.06.2019

---
+++ LEETCODE/912. Sort an Array.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/sort-an-array/
+
+# Given an array of integers nums, sort the array in ascending order.
+
+# Example 1:
+
+# Input: [5,2,3,1]
+# Output: [1,2,3,5]
+# Example 2:
+
+# Input: [5,1,1,2,0,0]
+# Output: [0,0,1,1,2,5]
+ 
+# Note:
+
+# 1 <= A.length <= 10000
+# -50000 <= A[i] <= 50000
+
+class Solution(object):
+    def sortArray(self, nums):
+        return sorted(nums)
