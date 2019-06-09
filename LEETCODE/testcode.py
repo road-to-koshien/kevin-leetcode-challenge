d7e098a2e98705473963cedd45deef40f2d78d55
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-09 22:50:13 +0200

    09.06.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,13 +1,6 @@
-# list1 = ['a','b','c']
-# list2 = ['b','c','d']
-# dict1 = dict.fromkeys(list1, 1)
-# dict2 = dict.fromkeys(list2, 1)
-# # dict1 &= dict2
-# t = list(dict1.elements())
-# print(t)
-from collections import Counter
-a = 'hahuythap'
-t = Counter(a)
-res = list(t.elements())
-print(res)
-print(t)
+import webbrowser
+
+origin = 'http://www.abcxyz.com/document'
+for i in range(1, 99999):
+    weblink = origin + '{0:05}'.format(i) + '.pdf'
+    webbrowser.open(weblink)
