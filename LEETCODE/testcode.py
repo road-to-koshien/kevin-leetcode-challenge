553af2ffac67bef0889f37b6d32019651a8ac8d7
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-11 15:18:02 +0200

    11.06.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,5 +1,19 @@
-a = [1,2,3,4,5]
-for i in range(0, len(a)):
-    a.pop()
-    print(len(a))
-print(a)
+# Given words first and second, consider occurrences in some text of the form "first second third", 
+# where second comes immediately after first, 
+# and third comes immediately after second.
+
+# For each such occurrence, add "third" to the answer, and return the answer.
+
+ 
+
+# Example 1:
+
+# Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+# Output: ["girl","student"]
+# Example 2:
+
+# Input: text = "we will we will rock you", first = "we", second = "will"
+# Output: ["we","rock"]
+
+a = 'ha huy thap'
+print(a.split())
