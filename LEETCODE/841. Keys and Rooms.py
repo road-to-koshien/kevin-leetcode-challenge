26095219b1bca5b837451f439300c6fa9126d7a5
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-17 13:33:42 +0200

    17.06.2019

--- LEETCODE/841. Keys and Rooms.py
+++ LEETCODE/841. Keys and Rooms.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/keys-and-rooms/
+
 # There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
 
 # Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
@@ -22,34 +24,17 @@
 # Input: [[1,3],[3,0,1],[2],[0]]
 # Output: false
 # Explanation: We can't enter the room with number 2.
-# Note:
 
-# 1 <= rooms.length <= 1000
-# 0 <= rooms[i].length <= 1000
-# The number of keys in all rooms combined is at most 3000.
-
-def roomandkeys(rooms):
-    keys = [0]
-    checked = set(keys)
-    while keys:
-        i = keys.pop()
-        for key in rooms[i]:
-            if key not in checked:
-                keys.append(key)
-        checked.add(i)
-        if len(checked) == len(rooms):
-            return True
-    return len(checked) == len(rooms)
-        
-rooms = [[1],[2],[3],[]]
-print(roomandkeys(rooms))
-
-
-  
-
-            
-            
-                
-        
-
-
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        keys = [0]
+        checked = set(keys)
+        while keys:
+            i = keys.pop()
+            for key in rooms[i]:
+                if key not in checked:
+                    keys.append(key)
+            checked.add(i)
+            if len(checked) == len(rooms):
+                return True
+        return len(checked) == len(rooms)
