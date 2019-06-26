97d47a36e22bd0910a2780cad0bf18bc8e509761
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-26 22:33:42 +0200

    26.06.2019

--- LEETCODE/testcode3.py
+++ LEETCODE/testcode3.py
@@ -1,6 +1,18 @@
-a = '123thap'
-for char in a:
-    if char.isalpha() == True:
-        continue
-    else:
-        print(char)
+import numpy as np
+from scipy.io import wavfile
+
+fs = 44.1e3
+t = np.arange(0, 1.0, 1.0/fs)
+
+f1 = 440
+f2 = 600
+
+x = 0.5*np.sin(2*np.pi*f1*t) + 0.5*np.sin(2*np.pi*f2*t)
+
+fname = 'output.wav'
+wavfile.write( fname, fs, x )
+
+
+fs, data = wavfile.read( fname )
+
+print(fs, data[:10])
