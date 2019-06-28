11b0e8e698a7970a68aa500d7efcfa39e1fbd970
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-28 15:57:15 +0200

    28.06.2019

--- LEETCODE/testcode3.py
+++ LEETCODE/testcode3.py
@@ -1,4 +1,25 @@
-import math
-a = 9
-x = math.ceil(math.sqrt(a))
-print(x)
+def findLongestWord(s, d):
+    collect = []
+    max_global = 0
+    for word in d:
+        t = 0
+        for char in s:
+            if char == word[t]:
+                t += 1
+                if t == len(word):
+                    if len(word) == max_global:
+                        collect.append(word)
+                    if len(word) > max_global:
+                        collect = []
+                        collect.append(word)
+                        max_global = len(word)
+                    break
+    if collect:
+        collect.sort()
+        return collect[0]
+    else:
+        return ''
+s = "abpcplea"
+d = ["a","b","c"]
+print(findLongestWord(s,d))
+
