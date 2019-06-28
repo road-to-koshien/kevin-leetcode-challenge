3cbad2bfa199505f542a99f75b17b52ab33e7119
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-06-28 07:23:51 +0200

    28.06.2019

--- LEETCODE/testcode.py
+++ LEETCODE/testcode.py
@@ -1,62 +1,11 @@
-def mergeSort(arr): 
-    print(arr)
-    if len(arr) >1: 
-        mid = len(arr)//2 #Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
-        # print(L, R)
+import math
+from numpy import cross, eye, dot
+from scipy.linalg import expm, norm
 
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
-        
-        # i = j = k = 0
+def M(axis, theta):
+    return expm(cross(eye(3), axis/norm(axis)*theta))
 
-        # while i < len(L) and j < len(R): 
-        #     if L[i] < R[j]: 
-        #         arr[k] = L[i] 
-        #         i+=1
-        #     else: 
-        #         arr[k] = R[j] 
-        #         j+=1
-        #     k+=1
+v, axis, theta = [4,4,4], [0,0,1], np.(math.pi/2.0)
+M0 = M(axis, theta)
 
-        # while i < len(L): 
-        #     arr[k] = L[i] 
-        #     i+=1
-        #     k+=1
-          
-        # while j < len(R): 
-        #     arr[k] = R[j] 
-        #     j+=1
-        #     k+=1
-    # print(arr)
-
-def printList(arr): 
-    for i in range(len(arr)):         
-        print(arr[i])
-    print() 
-  
-if __name__ == '__main__': 
-    arr = [8,7,6,5,4,3]  
-    print ("Given array is", )
-    # printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", )
-    # printList(arr) 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(dot(M0,v))
--- LEETCODE/testcode2.py
+++ LEETCODE/testcode2.py
@@ -1,6 +1,24 @@
-import sys
-sys.path.append(r'D:')
-from testclass import *
+import numpy as np
+import math
 
-test = person('John', 38)
-print(test.printinfo())
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta / 2.0)
+    b, c, d = -axis * math.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+v = [4, 4, 4]
+axis = [0, 0, 1]
+theta = 0.5
+
+print(np.dot(rotation_matrix(axis, theta), v)) 
+# [ 2.74911638  4.77180932  1.91629719]
