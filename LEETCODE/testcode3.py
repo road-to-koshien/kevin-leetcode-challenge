a91dfed5ed7158a192fc4a452934c0d69a04429d
mrkevinht <34190774+mrkevinht@users.noreply.github.com>
2019-07-03 07:52:49 +0200

    03.07.2019

--- LEETCODE/testcode3.py
+++ LEETCODE/testcode3.py
@@ -1,25 +1,7 @@
-def findShortestSubArray(nums):
-    from collections import Counter
-    x = Counter(nums)
-    max_global = -float('inf')
-    collect = []
-    for i,j in x.items():
-        if j == max_global:
-            collect.append(i)
-        elif j > max_global:
-            collect = []
-            collect.append(i)
-            max_global = j
-    print(collect)
-    min_global = float('inf')
-    for each in collect:
-        start = nums.index(each)
-        end = nums[::-1].index(each)
-        min_cur = len(nums) - end - start
-        if min_cur < min_global:
-            min_global = min_cur
-    return min_global
-
-nums = [1,1,2,2,2,1]
-print(findShortestSubArray(nums))
-                
+nums = [1,2,3,4]
+nums.sort(reverse = True)
+list_multi = nums[:3]
+res = 1
+for each in list_multi:
+    res = res * each
+print(res)
