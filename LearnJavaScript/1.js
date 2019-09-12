77f464b05503217032b228e7b0cf4f92e1edc354
Ha Huy Thap <mrkevinht@gmail.com>
2019-09-13 01:41:50 +0800

    up

---
+++ LearnJavaScript/1.js
@@ -0,0 +1,32 @@
+// var test_array = [1,2,3,4,5];
+// var first = test_array.shift();
+// console.log(first);
+// console.log(test_array);
+
+// test_array.unshift(10);
+// console.log(test_array);
+
+// function test(a,b){
+//     var c = a - b;
+//     return c;
+// }
+
+// console.log(test(100,5));
+
+// var test_object = {
+//     "Duyen" : "mom",
+//     "Hung" : "brother",
+//     "Thap" : "son"
+// };
+
+// console.log(test_object.Thap);
+
+var array = [];
+var i = 0;
+while (i<10){
+    array.push(i);
+    i++;
+}
+console.log(array);
+
+
